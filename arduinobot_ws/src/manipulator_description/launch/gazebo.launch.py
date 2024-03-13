13bd1f5d7012fb62f6b2026bfd3fa1d6122e4022
1Gughan1 <gughan.v.2003@gmail.com>
2024-03-13 20:01:08 +0530

    Add files via upload

--- arduinobot_ws/src/manipulator_description/launch/gazebo.launch.py
+++ arduinobot_ws/src/manipulator_description/launch/gazebo.launch.py
@@ -1,58 +1,35 @@
 import os
 from launch import LaunchDescription
+from launch.actions import DeclareLaunchArgument, ExecuteProcess
+from launch.substitutions import LaunchConfiguration
 from launch_ros.actions import Node
-from launch_ros.parameter_descriptions import ParameterValue
-from launch.actions import DeclareLaunchArgument, SetEnvironmentVariable, IncludeLaunchDescription
-from launch.substitutions import Command, LaunchConfiguration
-from ament_index_python.packages import get_package_share_directory
-from launch.launch_description_sources import PythonLaunchDescriptionSource
-
-from os import pathsep
 
 def generate_launch_description():
-    manipulator_description = get_package_share_directory("manipulator_description")
-    manipulator_description_prefix = get_package_share_directory("manipulator_description")
-    model_path = os.path.join(manipulator_description, "models")
-    model_path += pathsep + os.path.join(manipulator_description_prefix, "share")
 
-    env_variable = SetEnvironmentVariable("GAZEBO_MODEL_PATH", model_path)
-    model_arg = DeclareLaunchArgument(
-        name="model",
-        default_value=os.path.join(get_package_share_directory("manipulator_description"), "urdf", "manipulator.urdf.xacro"),
-        description="Absolute path to the robot URDF file"
-    )
-
-    robot_description = ParameterValue(Command (["xacro ", LaunchConfiguration ("model")]))
-
-    robot_state_publisher = Node(
-        package="robot_state_publisher",
-        executable="robot_state_publisher",
-        name="robot_state_publisher",
-        output="screen",
-        parameters=[{"robot_description": robot_description}],
-    )
-
-    start_gazebo_server = IncludeLaunchDescription(PythonLaunchDescriptionSource(os.path.join(
-        get_package_share_directory("gazebo_ros"), "launch", "gzserver.launch.py"
-    )))
-
-    start_gazebo_client = IncludeLaunchDescription(PythonLaunchDescriptionSource(os.path.join(
-        get_package_share_directory("gazebo_ros"), "launch", "gzclient.launch.py"
-    )))
-
-    spawn_robot = Node(
-        package="gazebo_ros",
-        executable="spawn_entity.py",
-        arguments=["-entity", "manipulator", "-topic", "robot_description"],
-        output="screen"
-    )
+    use_sim_time = LaunchConfiguration('use_sim_time', default='false')
+    urdf_absolute_path = "/home/ros2_foxy/arduinobot_ws/src/manipulator_description/urdf/arm.urdf.xacro"
 
     return LaunchDescription([
-        env_variable,
-        model_arg,
-        robot_state_publisher,
-        start_gazebo_client,
-        start_gazebo_server,
-        spawn_robot  
-    ])
+        DeclareLaunchArgument(
+            'use_sim_time',
+            default_value='false',
+            description='Use simulation (Gazebo) clock if true'
+        ),
 
+        Node(
+            package='robot_state_publisher',
+            executable='robot_state_publisher',
+            name='robot_state_publisher',
+            output='screen',
+            parameters=[{'use_sim_time': use_sim_time}],
+            arguments=[urdf_absolute_path]
+        ),
+
+        Node(
+            package='gazebo_ros',
+            executable='spawn_entity.py',
+            name='urdf_spawner',
+            output='screen',
+            arguments=["-topic", "/robot_description", "-entity", "robot_arm"]
+        )
+    ])
